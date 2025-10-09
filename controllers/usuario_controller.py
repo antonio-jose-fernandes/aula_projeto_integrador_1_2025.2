41d4c2f3d522d34b5b5cf3fde19aefe86463b8b6
antonio-jose-fernandes <110204549+antonio-jose-fernandes@users.noreply.github.com>
2025-10-09 16:30:52 -0300

    add listar

---
+++ controllers/usuario_controller.py
@@ -0,0 +1,39 @@
+from main import app
+from flask import request, render_template, redirect, url_for
+from models.usuario_model import *
+# Criando a sessão para interagir com o banco de dados
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+# rotas
+@app.route("/usuario/inserir")
+def inserir_usuario():
+   return render_template("usuario/cadastro.html")
+
+
+@app.route("/usuario/create", methods=['POST'])
+def create_usuario():
+ if request.method == 'POST':
+ # Captura os dados enviados pelo formulário
+    nome = request.form['nome']
+    login = request.form['login']
+    senha = request.form['senha']
+    email = request.form['email']
+    telefone = request.form['telefone']
+    # Cria um novo usuário
+    new_user = Usuario(nome=nome,login=login,senha=senha, email=email,
+    telefone=telefone)
+ # Cria um novo usuário no banco de dados
+ db = SessionLocal()
+ # Adiciona o novo usuário ao banco de dados
+ db.add(new_user)
+ db.commit()
+ return redirect(url_for('lista_usuario'))
+
+@app.route("/usuario/listar")
+def lista_usuario():
+   db = SessionLocal()
+   # Consultar todos os usuários
+   usuarios = db.query(Usuario).all()
+   # Renderizar o HTML passando os dados
+   return render_template('usuario/listar.html', usuarios_teste=usuarios)
--- main.py
+++ main.py
@@ -1,24 +1,5 @@
-from flask import Flask, render_template
-
+from flask import Flask
 app = Flask(__name__)
-
-@app.route('/')
-def ola_mundo():
-    return 'oi'
-
-@app.route('/login')
-def login_sistema():
-    return render_template('login.html')
-
-
-@app.route('/usuario/cadastros')
-def inserir_usuario():
-     return render_template('usuario/cadastro.html')
-
-@app.route('/usuario/lista')
-def lista_usuario():
-    return render_template('usuario/listar.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
-   
+from controllers.usuario_controller import *
+if __name__ == "__main__":
+  app.run()
